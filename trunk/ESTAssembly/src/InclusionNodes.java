c6c14dc25dc329fc1bdf9a2fab898164d8861f28
nwpuzebra <nwpuzebra@bb862024-198a-11de-b6aa-3535bc3264a2>
2009-09-29 00:58:08 +0000

    A version: records all the inclusion during the calculation to grandparent level and other further levels. Change processMoreConsensusWithInclusion function. Now use the one with longest first Est and the one using the most number of nodes and combine them together to get a new consensus.

--- trunk/ESTAssembly/src/InclusionNodes.java
+++ trunk/ESTAssembly/src/InclusionNodes.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Stack;
 import java.util.TreeSet;
+
+import com.mhhe.clrs2e.Vertex;
+import com.mhhe.clrs2e.WeightedAdjacencyListGraph;
+import com.mhhe.clrs2e.WeightedEdgeIterator;
 
 /*
  * record all the nodes which are included in other nodes.
@@ -8,51 +13,109 @@ import java.util.TreeSet;
  * we use them again during reconstruction the consensus.
  */
 public class InclusionNodes {
-	TreeSet<InclusionNode> nodes; 
-	//TreeSet<PNode> prtNodes; 
-	
+	TreeSet<InclusionNode> nodes; //Used in handleInclusion in Graph.java. The compared member variable has to be unique.
+	ArrayList<PairNode> nodes2; //Used in get2CloseNodesFromGrand and findAdjacentNode in Graph.java.
+	WeightedAdjacencyListGraph directedGraph;
+	TreeSet<PNode> pNodes; //store all the parent nodes in nodes and nodes2.
+
 	
 	public InclusionNodes() {
 		nodes = new TreeSet<InclusionNode>();
-		//prtNodes = new TreeSet<PNode>();
+		nodes2 = new ArrayList<PairNode>();
+		pNodes = new TreeSet<PNode>();
+		directedGraph = null;
 	}
 	
 	// chd is included in parent.
 	public void addNode(int chd, int parent) {
 		nodes.add(new InclusionNode(chd, parent));
-		//prtNodes.add(new PNode(parent, chd));
 	}
 	
+	public void addNode2(int chd, int parent) {
+		nodes2.add(new PairNode(chd, parent));
+	}
+
 	public int getSize() {
 		return nodes.size();
 	}
 	
 	/*
-	 * check if the idx is in the nodes.
+	 * check if the idx is in the "nodes".
 	 */
 	public boolean containInclusionNode(int idx) {
 		return nodes.contains(new InclusionNode(idx,0));
 	}
 
 	/*
-	 * If pIdx is in prtNodes, return the corresponding idxChd;
+	 * If pIdx is in nodes or nodes2, return the corresponding idxChd;
 	 * else return -1.
+	 * Called by addInclusionNodes in ESTAssembly.java.
 	 */
-	public int[] containPNode(int pIdx) {
-		ArrayList<Integer> ret = new ArrayList<Integer> ();
+	public int[] containPNode(int pIdx, int numOfTotalNodes) {
+		if (directedGraph == null) {
+			makeDirectedGraph(numOfTotalNodes);
+		}
+		
+		if (!pNodes.contains(new PNode(pIdx))) {
+			return null;
+		}
+		
+		Stack<Integer> allNodes = new Stack<Integer> ();
+//		Stack<Integer> partNodes = new Stack<Integer> ();
+//		partNodes.push(Integer.valueOf(pIdx));
+//		
+//		while (!partNodes.empty()) {
+//			Stack<Integer> tmpStack = new Stack<Integer> ();
+//			while (!partNodes.empty()) {
+//				int tmpIndex = partNodes.pop();
+//				WeightedEdgeIterator ite = (WeightedEdgeIterator) directedGraph.edgeIterator(tmpIndex);
+//				while (ite.hasNext()) {
+//					Vertex v = (Vertex) ite.next();
+//					int tIndex = v.getIndex();
+//					tmpStack.push(Integer.valueOf(tIndex));
+//					allNodes.push(Integer.valueOf(tIndex));
+//				}
+//			}
+//			partNodes = tmpStack;
+//		}
+		
+		WeightedEdgeIterator ite = (WeightedEdgeIterator) directedGraph.edgeIterator(pIdx);
+		while (ite.hasNext()) {
+			Vertex v = (Vertex) ite.next();
+			int tIndex = v.getIndex();
+			allNodes.push(Integer.valueOf(tIndex));
+		}
+
+		int[] values = new int[allNodes.size()];
+		for (int i=0; i<allNodes.size(); i++) {
+			values[i] = allNodes.get(i);
+		}
+
+		return values;		
+	}
+	
+	private void makeDirectedGraph(int numOfTotalNodes) {
+		// Make a directed graph.
+		int nOfNodes = numOfTotalNodes;
+		directedGraph = new WeightedAdjacencyListGraph(nOfNodes, true);
+		
+		for (int i=0; i<nOfNodes; i++) {
+			directedGraph.addVertex(i, Integer.toString(i));
+		}
+
 		Iterator<InclusionNode> ite = nodes.iterator();
 		while (ite.hasNext()) {
 			InclusionNode n1 = ite.next();
-			if (n1.idxP == pIdx) {
-				ret.add(Integer.valueOf(n1.idxChd));
-			} 
+			directedGraph.addEdge(n1.idxP, n1.idxChd, 1);
+			pNodes.add(new PNode(n1.idxP));
 		}
 		
-		int[] values = new int[ret.size()];
-		for (int i=0; i<ret.size(); i++) {
-			values[i] = ret.get(i);
+		Iterator<PairNode> ite2 = nodes2.iterator();
+		while (ite2.hasNext()) {
+			PairNode n1 = ite2.next();
+			directedGraph.addEdge(n1.idxP, n1.idxChd, 1);
+			pNodes.add(new PNode(n1.idxP));
 		}
-		return values;		
 	}
 	
 	public void printAllNodes() {
@@ -62,20 +125,26 @@ public class InclusionNodes {
 			InclusionNode n1 = ite.next();
 			System.out.println(n1.idxChd+"\t"+n1.idxP);
 		}
+		Iterator<PairNode> ite2 = nodes2.iterator();
+		while (ite2.hasNext()) {
+			PairNode n1 = ite2.next();
+			System.out.println(n1.idxChd+"\t"+n1.idxP);
+		}
 	}
 	
 	public static void main(String args[]) {
 		InclusionNodes in = new InclusionNodes();
-		in.addNode(3,5);
-		in.addNode(5,7);
-		in.addNode(7,7);
-		in.addNode(6,7);
+		in.addNode(4,3);
+		in.addNode(5,3);
+		in.addNode(6,4);
+		in.addNode(7,4);
+		in.addNode2(5,1);
+		in.addNode2(7,1);
 		
-		System.out.println(in.containInclusionNode(7));
-		int[] a = in.containPNode(7);
-		if (a.length == 0) {
-			System.out.println("false");
-		}
+		//System.out.println(in.containInclusionNode(7));
+		int[] a = in.containPNode(3, 8);
+		System.out.println(a.length);
+		//System.out.println(in.containInclusionNode(3));
 	}
 }
 
@@ -102,16 +171,21 @@ class InclusionNode implements Comparable<InclusionNode>{
 	}
 }
 
-/*
- * similar to InclusionNode except that the comparable variable is pNodes instead of curNode.
- */
-/*class PNode implements Comparable<PNode>{ 
+class PairNode { 
 	int idxP; //index of the node.
 	int idxChd;  //index of the node which is included by pNode.
 	
-	PNode(int p, int c) {
+	PairNode(int c, int p) {
 		idxP = p;
 		idxChd = c;
+	}
+}
+
+class PNode implements Comparable<PNode>{
+	int idxP;  //index of the node which include curNode.
+	
+	PNode(int p) {
+		idxP = p;
 	}
 	
 	public int compareTo(PNode other) {
@@ -126,4 +200,4 @@ class InclusionNode implements Comparable<InclusionNode>{
 			return -1;
 		}
 	}
-}*/
+}
